83da32bf8ef709ab021f65e1db75c8d5a8096d2e
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-06-06 16:32:02 -0300

    Add Like method

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,10 @@
 class Like < ActiveRecord::Base
   belongs_to :user, class_name: 'User'
   belongs_to :post, class_name: 'Post'
+
+  after_save :update_like_counter
+
+  def update_like_counter
+    post.increment!(:likes_counter)
+  end
 end
