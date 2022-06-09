ce93b99c5be1a10c05b2ef27f05413583665cbfc
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-06-09 15:53:50 -0300

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new] do
+    resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:create]
+      resources :likes, only: [:create]
     end
   end
-
-  post 'users/:user_id/posts', to: 'posts#create', as: 'posts'
-  post 'users/:user_id/posts/:post_id/likes', to: 'likes#create', as: 'likes'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
    root "users#index"
 end
