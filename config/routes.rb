96ebf52b8bca9562cced935a4aa4686e053b8810
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-06-14 16:49:08 -0300

    Setup Sendgrid using Devise for confirmation emails and add routes for Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'sign_up' }
+
+  devise_scope :user do
+    get '/sign_out', to: 'users/sessions#destroy'
+  end
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
