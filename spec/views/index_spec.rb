c5b8787ec372c8a1d97434f50b7bc9d5ae5d02e3
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-06-15 20:50:42 -0300

    Fix linter errors

--- spec/views/index_spec.rb
+++ spec/views/index_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :system do
   describe 'index' do
     before(:each) do
-      User.create(name: 'Test', email: 'a@a', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', password: '123456', posts_counter: 3,
-                  confirmed_at: Time.now)
-      User.create(name: 'Test2', email: 'b@b', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', password: '123456', posts_counter: 5,
-                  confirmed_at: Time.now)
+      User.create(name: 'Test', email: 'a@a', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                  password: '123456', posts_counter: 3, confirmed_at: Time.now)
+      User.create(name: 'Test2', email: 'b@b', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                  password: '123456', posts_counter: 5, confirmed_at: Time.now)
 
       visit new_user_session_path
 
